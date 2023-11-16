432b4a1d692cb28422a734f4865d44d5c451b402
abhay2002-pro <abhayray2002@gmail.com>
2023-11-16 11:23:21 +0530

    Initial setup done

---
+++ src/config/database-config.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const serverConfig = require('./server-config');
+
+const connectDB = async () => {
+    try{
+        mongoose.set("strictQuery", false);
+        const { connection } = await mongoose.connect(serverConfig.MONGODB_URI);
+        console.log(`MongoDB connected with ${connection.host}`);
+    } catch(err){
+        console.log(`Some error occured. Error message: ${err}`);
+    }
+};
+
+module.exports = {
+    connectDB
+}
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const { ServerConfig, DatabaseConfig } = require("./config");
+const apiRoutes = require("./routes");
+
+// connect to Database
+DatabaseConfig.connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// app.use("/api", apiRoutes);
+
+app.listen(ServerConfig.PORT, () => {
+  console.log(`Sucessfully started the server on PORT : ${ServerConfig.PORT}`);
+});
